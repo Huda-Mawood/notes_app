314ac85b036f0bfe5b6da302f60465b4dfd81e6d
Huda-Mawood <huda.mawod881@compit.aun.edu.eg>
2025-01-10 10:39:51 +0200

    add border radius to bottom sheet

--- lib/views/widgets/custom_search_icon.dart
+++ lib/views/widgets/custom_search_icon.dart
@@ -9,13 +9,14 @@ class CustomSearchIcon extends StatelessWidget {
       height: 45,
       width: 45,
       decoration: BoxDecoration(
-        color: Colors.white..withValues(alpha: .05),
+        color: Colors.black.withValues(alpha: .45),
         borderRadius: BorderRadius.circular(16),
       ),
       child: const Center(
         child: Icon(
           Icons.search,
           size: 28,
+          color: Colors.white,
         ),
       ),
     );
